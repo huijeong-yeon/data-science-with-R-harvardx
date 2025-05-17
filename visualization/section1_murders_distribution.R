9c44579160fc100f22a41b8d36397a0f0788a3a7
Huijeong Sophie Yeon <byngohj@gmail.com>
2025-05-18 00:22:45 +0900

    Update section1_murders_distribution.R

--- visualization/section1_murders_distribution.R
+++ visualization/section1_murders_distribution.R
@@ -29,3 +29,31 @@ heights %>%
   stat_ecdf() +
   ylab("F(a)") + 
   xlab("a")
+
+# Proportion of males shorter than 75 inches
+heights %>%
+  filter(sex == "Male") %>%
+  summarize(p = mean(height < 75))
+
+# Calculate median height of male students
+heights %>%
+  filter(sex == "Male") %>%
+  summarize(median_height = median(height))
+
+# Calculate and count states with murder rate > 10
+sum((murders$total / murders$population) * 100000 > 10)
+
+# Count the number of males with height between 62.5 and 65.5
+heights %>%
+  filter(sex == "Male", height >= 62.5, height <= 65.5) %>%
+  summarize(count = n())
+
+# Percentage of males shorter than 60
+heights %>%
+  filter(sex == "Male") %>%
+  summarize(p = mean(height < 60))
+
+# proportion of US states that have populations larger than 10 million?
+mean(murders$population > 10^7)
+
+
